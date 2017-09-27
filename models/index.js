da6ae1c10d934bb5dcd5db077e5dfc3bf0a9dfed
DESKTOP-F2GE9JV\Jose Vinicius <josevini29@hotmail.com>
2017-09-27 20:57:34 -0300

    Aula 27/09/2017

---
+++ models/index.js
@@ -0,0 +1,28 @@
+const Sequelize = require('sequelize');
+
+const config = {
+    dialect: 'sqlite',
+    storage: './banco.sqlite',
+    define: {
+        freezeTableName: true,
+        timestamps: false
+    },
+
+};
+
+const sequelize = new Sequelize(null, null, null, config);
+
+const Todo = sequelize.define('todo', {
+    id: {
+        primaryKey: true,
+        type: Sequelize.INTEGER
+    },
+    title: Sequelize.STRING(50),
+    description: Sequelize.STRING(300),
+    completed: Sequelize.BOOLEAN,
+    creation_date: Sequelize.DATE
+})
+
+module.exports = {
+    sequelize, Todo
+}
